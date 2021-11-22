0e4fd2baf69160684b58ea234b4ce1b690ec296b
Jefferson <jefferson-arsene-minakpon.vodounnou@epitech.eu>
2021-11-22 15:53:44 +0100

    Added validation and styles to create ads template.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
                                 <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Dashboard</a></li>
                                 <li><a class="dropdown-item" href="#">View my ads</a></li>
-                                <li><a class="dropdown-item" href="#">Create new ad</a></li>
+                                <li><a class="dropdown-item" href="/user/ads/create">Create new ad</a></li>
                                 <li><hr class="dropdown-divider"></li>
                                 <li><a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
--- resources/views/user/ads/create.blade.php
+++ resources/views/user/ads/create.blade.php
@@ -1,50 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Create new ad') }}</div>
 
-<form enctype="multipart/form-data" class="container my-5" action="/ads" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="title">Title</label>
-        <input name="title" type="text" class="form-control" id="title">
-    </div>
-    <div class="form-group">
-        <label for="description">Description</label>
-        <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="price">Price</label>
-        <input name="price" type="number" min="0" class="form-control" id="price">
-    </div>
-    <div class="form-group">
-        <label for="location">Location</label>
-        <input name="location" type="text" class="form-control" id="location">
-    </div>
-    <div class="custom-file mb-3 has-validation">
-        <input type="file" class="custom-file-input" id="picture" name="picture">
-        <label class="custom-file-label" for="picture">Choose a picture...</label>
-        {{-- <div class="invalid-feedback">Example invalid custom file feedback</div> --}}
-        {{-- <label for="picture">Picture</label>
-        <input name="picture" type="text" class="form-control" id="picture"> --}}
-    </div>
-    <div class="form-group">
-        <label for="category_id">Category</label>
-        <select class="custom-select" id="category_id" name="category_id">
-            <option selected disabled value="">Choose...</option>
-            <option value="">First</option>
-        </select>
-        {{-- <div class="invalid-tooltip">
-            Please select a valid state.
-        </div> --}}
-    </div>
-    
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+                <div class="card-body">
+                    <form enctype="multipart/form-data" action="/ads" method="POST">
+                        @csrf
+                        <div class="mb-4">
+                            <label class="form-label" for="title">Title</label>
+                            <input name="title" type="text" class="form-control @error('title') is-invalid @enderror" id="title" required>
+                            @error('title')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="description">Description</label>
+                            <textarea required class="form-control @error('description') is-invalid @enderror" name="description" id="description" cols="30" rows="10"></textarea>
+                            @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="price">Price</label>
+                            <input required name="price" type="number" min="0" class="form-control @error('price') is-invalid @enderror" id="price">
+                            @error('price')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="location">Location</label>
+                            <input required name="location" type="text" class="form-control @error('location') is-invalid @enderror" id="location">
+                            @error('location')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label for="picture" class="form-label">Picture</label>
+                            <input required class="form-control @error('picture') is-invalid @enderror" type="file" id="picture" name="picture">
+                            @error('picture')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="category_id">Category</label>
+                            <select class="form-select" id="category_id" name="category_id">
+                                <option selected disabled value="">Choose...</option>
+                                <option value="">First</option>
+                            </select>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <p>{{ $error }}</p>
-    @endforeach
-@endif
-
+                    {{-- @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    @endif --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
